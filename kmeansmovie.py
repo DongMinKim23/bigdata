68a26ae67d146e4173da9437a226b175ab073e3f
DongMinKim23 <kdm0615@gmail.com>
2019-10-28 14:04:54 +0900

    빅데이터 공부중

---
+++ kmeansmovie.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
